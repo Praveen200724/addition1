74822428d0306c14e5e0367ec53b176f9c5b5977
Praveen200724 <mishrapraveen2407@gmail.com>
2025-08-25 17:36:35 +0530

    airthmetic operations

---
+++ first.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d,e,f;
+	a = 10;
+	b = 67;
+	c = a+b;
+	d = a-b;
+	e = a*b;
+	f = a/b;
+	printf("the sum of %d and %d is %d \n",a,b,c);
+	printf("the sub of %d and %d is %d \n",a,b,d);
+	printf("the multi of %d and %d is %d \n",a,b,e);
+	printf("the div of %d and %d is %d",a,b,f);
+}
